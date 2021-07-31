6d44ba3f0cb853850a5655cbdff20a9842dc310b
Leonardo404-code <leonardobispo1000@gmail.com>
2021-07-31 00:15:55 -0300

    Added more information about BMI, added README

---
+++ README.md
@@ -0,0 +1,14 @@
+# BMI Calculation
+
+Application for study purposes with HTML, CSS and JavaScript vanila
+
+# How use
+
+- Clone the repository
+
+```
+git clone git@github.com:Leonardo404-code/IMC-Calc.git
+```
+
+- Open the HTML file
+- Enjoy the project :)
--- assets/back.js
+++ assets/back.js
@@ -23,26 +23,26 @@ form.addEventListener("submit", function (e) {
 
   if (IMC <= 18.5) {
     Result.className = "lowWeight";
-    Result.innerHTML = "Abaixo do peso";
+    Result.innerHTML = `<p>Abaixo do peso <br/> Seu IMC é: ${IMC}</p> <img src="../images/hungry.svg"/>`;
   }
   if (IMC > 18.5 && IMC <= 24.9) {
     Result.className = "normalWeight";
-    Result.innerHTML = "Peso normal";
+    Result.innerHTML = `<p>Peso normal <br/> Seu IMC é: ${IMC}</p> <img src="../images/heartbeat.svg"/>`
   }
   if (IMC > 25 && IMC <= 29.9) {
     Result.className = "overWeight";
-    Result.innerHTML = "Sobrepeso";
+    Result.innerHTML = `<p>Sobrepeso <br/> Seu IMC é: ${IMC}</p> <img src="../images/fat.svg"/>`;
   }
   if (IMC > 30 && IMC <= 34.9) {
     Result.className = "obesity";
-    Result.innerHTML = "Obesidade Grau 1";
+    Result.innerHTML = `<p>Obesidade Grau 1 <br/> Seu IMC é: ${IMC}</p> <img src="../images/obestity.svg"/>`;
   }
   if (IMC > 35 && IMC <= 39.9) {
     Result.className = "obesity";
-    Result.innerHTML = "Obesidade Grau 2";
+    Result.innerHTML = `<p>Obesidade Grau 2 <br/> Seu IMC é: ${IMC}</p> <img src="../images/obestity.svg"/>`;
   }
   if (IMC > 40) {
     Result.className = "obesity";
-    Result.innerHTML = "Obesidade Grau 3";
+    Result.innerHTML = `<p>Obesidade Grau 3 <br/> Seu IMC é: ${IMC}</p> <img src="../images/obestity.svg"/>`;
   }
 });
